2742c00c01f39e164f5197ff98f3d7a56da18d31
Yaser Jarrar <yaser@grout.io>
2014-10-04 19:01:05 +0400

    Update package def syntax for Meteor 0.9

--- package.js
+++ package.js
@@ -2,8 +2,8 @@ Package.describe({
   summary: "Provide security against active attacker"
 });
 
-Package.on_use(function(api) {
-    api.add_files('active.js', ['client','server']);
+Package.onUse(function (api) {
+    api.addFiles('active.js', ['client', 'server']);
 
     api.export("MYLAR_ACTIVE_ATTACKER");
 });
